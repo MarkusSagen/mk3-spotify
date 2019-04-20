6e107783cee85000081031079ccad941e601d8f9
Mk3Johnson <markus.johnson.93@gmail.com>
2019-04-20 19:25:18 +0200

    added redirect to server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import beeGee from './img/bee_gee.jpeg';
+import querystring from 'query-string';
 import './App.css';
+
+// Import Material-UI for buttons
+import { withStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import Button from '@material-ui/core/Button';
 
 // Styles
 let defaultTextColor = '#fff';
@@ -9,6 +14,23 @@ let defaultStyle = {
   color: defaultTextColor,
   display: 'inline-block',
 }
+
+let signInButtonStyle = withStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    width: 400,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+  label: {
+    textTransform: 'capitalize',
+  },
+})(Fab);;
+
 
 
 // Fake data
@@ -143,6 +165,9 @@ class App extends Component {
 
   // Await everything to load and set dependent states
   componentDidMount(){
+    // Parse AccessToken from Spotify API in seach bar
+    let parsed = querystring.parsed();
+
     // Wait 1 secound and then displays data
     /*
     setTimeout( () => {
@@ -196,17 +221,19 @@ class App extends Component {
               <h4> Sign in to test the features </h4>
               
               <div className="signInForm" style={{'textTransform': "uppercase"}}>
-                <button type="button" style={{'backgroundColor': 'MediumSeaGreen', 'borderColor': 'none', 
-                                              'height': '50px', 'width': '150px', 'borderRadius': '30px'}}>
-                  <p> Sign in</p>
-                </button>
+                <Fab onClick={() => 
+                  window.location = 'http://localhost:8888/login'
+                } classes={signInButtonStyle} variant="extended" size="medium" width="120px" color="primary" aria-label="Add">
+                  Extended
+                </Fab>
               </div>
+
               
             </div>
           </div>
         }
         </header>
-
+        <p className="reference" style={{}}> Made by Markus - 2019</p>
       </div>
 
     );
